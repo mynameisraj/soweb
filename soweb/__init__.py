967f987df8eae5e6c9be2bc2e4ea80e4ce334721
richard <rlee46@illinois.edu>
2015-11-04 19:04:01 -0600

    wrote an example endpoint

--- soweb/__init__.py
+++ soweb/__init__.py
@@ -1,15 +1,13 @@
 import os
 from flask import Flask, send_from_directory, render_template
+from flask_restful import Api
 
 #normal boilerplate
 app = Flask(__name__)
 app.config.from_object('config')
+api = Api(app)
 
-#register modules here
-#example:
-#from app.users.views import mod as usersModule
-#app.register_blueprint(usersModule)
-
+#basic routes
 @app.route("/favicon.ico")
 def favicon():
     return send_from_directory(
@@ -19,11 +17,6 @@ def favicon():
 def index():
     return render_template("index.html")
 
-@app.before_request
-def log_entry():
-    app.logger.debug("Handling request")
-
-
-@app.teardown_request
-def log_exit(exc):
-    app.logger.debug("Finished handling request", exc_info=exc)
+#rest endpoints
+from endpoints.example import Example
+api.add_resource(Example, "/example")
